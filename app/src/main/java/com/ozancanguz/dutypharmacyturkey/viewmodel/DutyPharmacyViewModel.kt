b32294448517b01811a364c3d22f29087a95f258
Ozan <ozancangu24@gmail.com>
2023-01-20 21:56:47 +0300

    -data requested in viewmodel

---
+++ app/src/main/java/com/ozancanguz/dutypharmacyturkey/viewmodel/DutyPharmacyViewModel.kt
@@ -0,0 +1,32 @@
+package com.ozancanguz.dutypharmacyturkey.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ozancanguz.dutypharmacyturkey.data.model.Pharmacy
+import com.ozancanguz.dutypharmacyturkey.data.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class DutyPharmacyViewModel(private val repository: Repository,application: Application):AndroidViewModel(application) {
+
+
+    private var dutyPharmacyList=MutableLiveData<Pharmacy>()
+
+    fun requestApiData(il:String){
+
+        viewModelScope.launch {
+            val response=repository.remote.getDutyPharmacy(il)
+            if(response.isSuccessful){
+                dutyPharmacyList.postValue(response.body())
+            }else{
+                Log.d("viewmodel","NO DATA")
+            }
+        }
+    }
+
+
+}
