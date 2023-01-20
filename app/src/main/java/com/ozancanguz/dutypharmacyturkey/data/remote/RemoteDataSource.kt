c31c89e62df5258a664dfe4291db09eb028e30e7
Ozan <ozancangu24@gmail.com>
2023-01-21 00:16:27 +0300

    -remote data source implement for kktc pharmacy

--- app/src/main/java/com/ozancanguz/dutypharmacyturkey/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/ozancanguz/dutypharmacyturkey/data/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.ozancanguz.dutypharmacyturkey.data.remote
 
 import com.ozancanguz.dutypharmacyturkey.data.api.PharmacyApi
+import com.ozancanguz.dutypharmacyturkey.data.model.kktc.KktcPharmacy
 import com.ozancanguz.dutypharmacyturkey.data.model.turkey.Pharmacy
 import retrofit2.Response
 import javax.inject.Inject
@@ -10,5 +11,10 @@ class RemoteDataSource@Inject constructor(private val pharmacyApi: PharmacyApi) 
 
     suspend fun getDutyPharmacy(il:String): Response<Pharmacy> {
    return     pharmacyApi.getDutyPharmacy(il)
+    }
+
+
+    suspend fun getkktcPharmacy(city:String):Response<KktcPharmacy>{
+       return pharmacyApi.getkktcPharmacy(city)
     }
 }
