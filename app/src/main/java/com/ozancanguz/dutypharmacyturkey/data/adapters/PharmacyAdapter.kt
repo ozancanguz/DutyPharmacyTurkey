105d173a3f03835c0a1f80f9b0a9ac4b91afed44
Ozan <ozancangu24@gmail.com>
2023-01-20 22:07:39 +0300

    -pharmacy adapter created

---
+++ app/src/main/java/com/ozancanguz/dutypharmacyturkey/data/adapters/PharmacyAdapter.kt
@@ -0,0 +1,39 @@
+package com.ozancanguz.dutypharmacyturkey.data.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.dutypharmacyturkey.R
+import com.ozancanguz.dutypharmacyturkey.data.model.Pharmacy
+import kotlinx.android.synthetic.main.rv.view.*
+
+class PharmacyAdapter:RecyclerView.Adapter<PharmacyAdapter.PharmacyViewHolder>() {
+
+    var pharmacyList= emptyList<com.ozancanguz.dutypharmacyturkey.data.model.Result>()
+
+    fun setData(newData:Pharmacy){
+        this.pharmacyList=newData.result
+    }
+
+    class PharmacyViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PharmacyViewHolder {
+       val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.rv,parent,false)
+        return  PharmacyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PharmacyViewHolder, position: Int) {
+       val currentPharmacy=pharmacyList[position]
+        holder.itemView.iltextview.text=currentPharmacy.name
+        holder.itemView.image_view.setImageResource(R.drawable.img)
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
