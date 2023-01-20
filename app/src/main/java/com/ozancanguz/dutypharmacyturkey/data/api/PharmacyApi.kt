a4681a9185522eeed45f5b0bbb6aa79630250c45
Ozan <ozancangu24@gmail.com>
2023-01-21 00:15:18 +0300

    -api interface implemented for kktc pharmacy

--- app/src/main/java/com/ozancanguz/dutypharmacyturkey/data/api/PharmacyApi.kt
+++ app/src/main/java/com/ozancanguz/dutypharmacyturkey/data/api/PharmacyApi.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.dutypharmacyturkey.data.api
 
+import com.ozancanguz.dutypharmacyturkey.data.model.kktc.KktcPharmacy
 import com.ozancanguz.dutypharmacyturkey.data.model.turkey.Pharmacy
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,12 +9,27 @@ import retrofit2.http.Query
 
 interface PharmacyApi {
 
+    // for turkey
     @Headers(
         "authorization: apikey 4S9qz1vJllFEUXdg2X6UxT:00p5DeCL7r6zj9xXkIoKmw",
         "content-type: application/json"
     )
     @GET("dutyPharmacy")
     suspend fun getDutyPharmacy(@Query("il")il:String):Response<Pharmacy>
+
+
+
+    // for kktc
+
+    @Headers(
+        "authorization: apikey 4S9qz1vJllFEUXdg2X6UxT:00p5DeCL7r6zj9xXkIoKmw",
+        "content-type: application/json"
+    )
+    @GET("kktcDutyPharmacy")
+    suspend fun getkktcPharmacy(@Query("city")city:String):Response<KktcPharmacy>
+
+
+
 
 
 }
